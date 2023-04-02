4f27f4d9ebc51af7d4422d358053fa57368827fd
S2T Candidate <jobs@s2t.ai>
2023-04-03 03:28:29 +0800

    Removed details screen

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from './screens/HomeScreen';
-import DetailsScreen from './screens/DetailsScreen';
 import LeaderboardsScreen from './screens/LeaderboardsScreen';
 import PlayScreen from './screens/PlayScreen';
 import SummaryScreen from './screens/SummaryScreen';
@@ -44,12 +43,6 @@ export default () => {
                     name="Leaderboards" 
                     component={LeaderboardsScreen}
                     options={{ title: 'Leaderboards' }}
-                />
-                <Stack.Screen 
-                    name="Details" 
-                    component={DetailsScreen}
-                    initialParams={{ userId: 420 }}
-                    options={{ title: 'My Details' }}
                 />
                 <Stack.Screen 
                     name="Home" 
